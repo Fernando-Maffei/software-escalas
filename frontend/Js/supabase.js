85cfc01fa5bad718a2fd5bbf0ff8046cda42d866
Fernando-Maffei <fernandmaffei@gmail.com>
2026-03-13 11:41:36 -0300

    Corrige caminhos dos arquivos para GitHub Pages

--- frontend/Js/supabase.js
+++ frontend/Js/supabase.js
@@ -1,9 +1,8 @@
 // frontend/Js/supabase.js
 
-const SUPABASE_URL = 'https://esazzdvufhbbdxjlezfr.supabase.co'
+const SUPABASE_URL = 'https://esazzvdufhbbdxjlezfr.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_f65yJGDBTvqpUpNKAanyog_oZSgdz7B'
 
-// Criar cliente Supabase e disponibilizar globalmente
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 window.supabaseClient = supabaseClient
 
